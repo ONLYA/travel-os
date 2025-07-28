13af4652757551848671cf9d1ed67d534695aca2
Xiaosheng An <36487580+ONLYA@users.noreply.github.com>
2025-07-28 22:06:26 +0000

    build.sh: add freecad-nightly, kicad and neovim

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux kicad neovim fd-find ripgrep
 
 # Use a COPR Example:
 #
@@ -18,6 +18,13 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+dnf5 -y copr enable @freecad/nightly
+dnf5 -y install freecad
+dnf5 -y copr disable @freecad/nightly
+
+dnf5 -y config-manager addrepo --from-repofile=https://repo.librewolf.net/librewolf.repo
+dnf5 install -y librewolf
 
 #### Example for enabling a System Unit File
 
